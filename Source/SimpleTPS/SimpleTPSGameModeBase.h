150bfdd7239ce924a9f4d31b6cd767413ab1eab9
juntaow0 <juntaowang1998@outlook.com>
2021-07-04 01:58:38 -0700

    create project

---
+++ Source/SimpleTPS/SimpleTPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleTPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLETPS_API ASimpleTPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
